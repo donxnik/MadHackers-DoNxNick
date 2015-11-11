6392cd049164aa1fdbb139208ef2c1a3319754ad
Madhackers21 <avtandil1994@mail.ru>
2015-11-11 21:32:21 +0400

    ::: 11.11.2015 ::
    1) Uploaded All VS2013 Files
    
    MyWork:::
    
    . Manage Tabs as New richTXT files (Using Reach TextBoxes)
    . Fonts FileMenu
    . BackGround FileMenu

---
+++ VS2013Files/G15Notepad/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace G15Notepad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quickDesktopToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.myComputerToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quickDesktopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.myComputerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.searchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.searchToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(603, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.quickDesktopToolStripMenuItem1,
+            this.myComputerToolStripMenuItem1});
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // quickDesktopToolStripMenuItem1
+            // 
+            this.quickDesktopToolStripMenuItem1.Name = "quickDesktopToolStripMenuItem1";
+            this.quickDesktopToolStripMenuItem1.Size = new System.Drawing.Size(156, 22);
+            this.quickDesktopToolStripMenuItem1.Text = "Quick(Desktop)";
+            this.quickDesktopToolStripMenuItem1.Click += new System.EventHandler(this.quickDesktopToolStripMenuItem1_Click);
+            // 
+            // myComputerToolStripMenuItem1
+            // 
+            this.myComputerToolStripMenuItem1.Name = "myComputerToolStripMenuItem1";
+            this.myComputerToolStripMenuItem1.Size = new System.Drawing.Size(156, 22);
+            this.myComputerToolStripMenuItem1.Text = "My Computer";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.quickDesktopToolStripMenuItem,
+            this.myComputerToolStripMenuItem});
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // quickDesktopToolStripMenuItem
+            // 
+            this.quickDesktopToolStripMenuItem.Name = "quickDesktopToolStripMenuItem";
+            this.quickDesktopToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.quickDesktopToolStripMenuItem.Text = "Quick(Desktop)";
+            this.quickDesktopToolStripMenuItem.Click += new System.EventHandler(this.quickDesktopToolStripMenuItem_Click);
+            // 
+            // myComputerToolStripMenuItem
+            // 
+            this.myComputerToolStripMenuItem.Name = "myComputerToolStripMenuItem";
+            this.myComputerToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.myComputerToolStripMenuItem.Text = "My Computer";
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // searchToolStripMenuItem
+            // 
+            this.searchToolStripMenuItem.Name = "searchToolStripMenuItem";
+            this.searchToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.searchToolStripMenuItem.Text = "Search";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 24);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(603, 345);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 369);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "G15Notepad";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quickDesktopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem myComputerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quickDesktopToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem myComputerToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem searchToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ VS2013Files/G15Notepad/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace G15Notepad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // იდეა 1.  იყოს ისეთი შესანახი სისტემა სადაც მოითხოვს პაროლის დამატებასაც
+            // ანუ სპეცალური საწყობი სადაც შენ არ გჭირდება მითითება თუ საიდან უნდა ჩაწეროს ან საიდან უნდა ამოიღოს.
+            // ან შესაძლებელია იყოს სკრიპტული 
+
+            // იდეა 2. background  და font ების ფერების შეცვლის ფუნქციის დამატება. და ფონტების ზომის და სტილის შეცვლის ფუნქციაც
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void quickDesktopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            //ინახავს დესკტოპზე თუმცა დროებით გადავდეთ C-ის რომელიღაც ფოლდერში რადგან ისე აკრძალულია
+            StreamWriter str = new StreamWriter((new FileStream(@"C:\Csharp\G15Text.txt", FileMode.Create)));
+            str.Write(richTextBox1.Text);
+            str.Close();
+            MessageBox.Show("File Have Been Sucessfuly Saved", "Confirm", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void quickDesktopToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            //ვამოწმებთ თუ რამე გვიწერია ტექსტში გახხსნამდე გამოიტანოს გაფრთხილება შეინაოხს თუ არა ეს ტექსტი
+            if(richTextBox1.TextLength>0)
+            {
+                MessageBox.Show("Save this text", "Warning", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+
+                
+            }
+            StreamReader str = new StreamReader((new FileStream(@"C:\Csharp\G15text.txt", FileMode.Open)));
+            richTextBox1.Text = str.ReadLine();
+            str.Close();
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(richTextBox1.TextLength>0)
+            {
+                MessageBox.Show("Are You Sure To Quit?", "Exit", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+                
+            }
+            this.Close();
+            
+            
+        }
+    }
+}
